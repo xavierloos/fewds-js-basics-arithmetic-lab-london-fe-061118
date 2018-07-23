bf14f8667ee7a274c177f48b1093034ffcd5d48b
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-23 16:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 let newID = oldID;
-var ageIsValid;
+var ageIsValid=true;
 if (ageIsValid===true){
   
 }
