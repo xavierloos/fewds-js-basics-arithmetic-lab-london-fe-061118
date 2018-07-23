75b267b8152ee58c41024845043e879e5652cb10
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-23 16:18:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 let newID = oldID;
 var ageIsValid = true;
+
 if (ageIsValid===true){
-  ageIsValid=Math.isInteger();
+  currentAge=Math.isInteger()
 }
 var randomNumber = Math.random();
