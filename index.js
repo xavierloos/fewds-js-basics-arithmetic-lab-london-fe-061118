b394dad64df8ad89d0893bdcdc3b4feee55ad254
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-23 16:08:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 let newID = oldID;
-var ramdomNumber=MATH.ramdom();
+var randomNumber=MATH.random();
